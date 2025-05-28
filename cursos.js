5291e753251f80e738f52415930cc67036e0d5bb
Leozinho <leandrolimapeixoto13@gmail.com>
2025-05-28 00:15:45 -0300

    fgaet

--- cursos.js
+++ cursos.js
@@ -11,21 +11,21 @@ document.addEventListener("DOMContentLoaded", () => {
     e.preventDefault();
 
     const nome = document.getElementById("nomeCurso").value.trim();
-const descricao = document.getElementById("descricaoCurso").value.trim();
-const carga = document.getElementById("cargaHoraria").value.trim();
+    const descricao = document.getElementById("descricaoCurso").value.trim();
+    const carga = document.getElementById("cargaHoraria").value.trim();
 
-if (!nome || !descricao || !carga) {
-  alert("Preencha todos os campos corretamente.");
-  return;
-}
+    if (!nome || !descricao || !carga) {
+      alert("Preencha todos os campos corretamente.");
+      return;
+    }
 
-const cursos = JSON.parse(localStorage.getItem("cursos")) || [];
-cursos.push({ nome, descricao, carga });
-
+    const cursos = JSON.parse(localStorage.getItem("cursos")) || [];
+    cursos.push({ nome, descricao, carga });
     localStorage.setItem("cursos", JSON.stringify(cursos));
 
     alert("Curso cadastrado com sucesso!");
     form.reset();
   });
 });
+
 
